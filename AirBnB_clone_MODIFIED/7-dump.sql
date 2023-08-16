368f70b6f4014c4d1faf6362670598066dc08417
toleman84 <gdponcemarsiglia@gmail.com>
2023-08-16 14:19:11 -0300

    fix

--- AirBnB_clone_MODIFIED/7-dump.sql
+++ AirBnB_clone_MODIFIED/7-dump.sql
@@ -36,11 +36,11 @@ DROP TABLE IF EXISTS `cities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cities` (
-  `id` varchar(60) NOT NULL,
+  `id` INT NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(128) NOT NULL,
-  `state_id` varchar(60) NOT NULL,
+  `state_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   KEY `state_id` (`state_id`),
   CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
@@ -65,7 +65,7 @@ DROP TABLE IF EXISTS `states`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `states` (
-  `id` varchar(60) NOT NULL,
+  `id` INT NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `name` varchar(128) NOT NULL,
